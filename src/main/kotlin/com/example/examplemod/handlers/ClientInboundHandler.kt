36b721d3b4cfc369d7b58fc8529ecf52835d29f3
Andrea Tupini <tupini07@gmail.com>
2022-12-21 17:21:40 -0600

    buffered write of packets

--- src/main/kotlin/com/example/examplemod/handlers/ClientInboundHandler.kt
+++ src/main/kotlin/com/example/examplemod/handlers/ClientInboundHandler.kt
@@ -1,8 +1,6 @@
 package com.example.examplemod.handlers
 
 import com.example.examplemod.extensions.copyToByteBufferSafe
-import com.example.examplemod.extensions.into
-import com.example.examplemod.utils.ExPacketDecoder
 import com.example.examplemod.utils.ExPacketEncoder
 import com.mojang.authlib.minecraft.client.ObjectMapper
 import com.mojang.logging.LogUtils
@@ -13,13 +11,15 @@ import io.netty.channel.SimpleChannelInboundHandler
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.PacketFlow
 import org.apache.commons.io.FileUtils
+import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.nio.ByteBuffer
 
 @ChannelHandler.Sharable
 class ClientInboundHandler(name: String) : SimpleChannelInboundHandler<Any>(false) {
-    val fileWriter: FileWriter
+    private val bufferedWriter: BufferedWriter
+    private var numWrites = 0
 
     init {
 
@@ -31,41 +31,56 @@ class ClientInboundHandler(name: String) : SimpleChannelInboundHandler<Any>(fals
             outputFile.createNewFile()
         }
 
-        fileWriter = FileWriter(outputFile)
+        // NOTE: writes happen on the network thread, so it might cause the game to lag. Better to move this
+        // to a separate thread
+        bufferedWriter = BufferedWriter(FileWriter(outputFile, Charsets.UTF_8), 512_000)
     }
 
     override fun channelRead0(ctx: ChannelHandlerContext?, msg: Any?) {
+        // TODO: How do we know which `tick` number the current event happened in?
 
         if (msg is ByteBuf) {
             val byteBuffer: ByteBuffer = msg.copyToByteBufferSafe()
             val packet = serializer.writeValueAsString(byteBuffer)
-            fileWriter.write(packet)
+
+            bufferedWriter.write(packet)
         } else {
             try {
                 val penc = ExPacketEncoder(PacketFlow.CLIENTBOUND)
                 val encoded = penc.exEncode(ctx!!, msg as Packet<*>)
 
+                // NOTE: it's a bit weird but the `serialized` output is enclosed in double quotes, and these are
+                // necessary for the deserializer to work properly!
                 val serialized = serializer.writeValueAsString(encoded!!.copyToByteBufferSafe())
 
-                // verify that deserialization works properly as well
-                val pdec = ExPacketDecoder(PacketFlow.CLIENTBOUND)
-                val decoded = pdec.exDecode(ctx, encoded.copy())
+                // NOTE: The below can be used to re-read the "serialized" packet
+//                // verify that deserialization works properly as well
+//                val pdec = ExPacketDecoder(PacketFlow.CLIENTBOUND)
+//                val justDecoded = pdec.exDecode(ctx, encoded.copy())
+//
+//                // try to re-decode
+//                val deserializedAndDecoded = serializer
+//                    .readValue(serialized, ByteBuffer::class.java)
+//                    .let {
+//                        pdec.exDecode(ctx, it.into<ByteBuf>())
+//                    }
 
-                // try to re-decode
-                val deserialized = serializer
-                    .readValue(serialized, ByteBuffer::class.java)
-                    .let {
-                        pdec.exDecode(ctx, it.into<ByteBuf>())
-                    }
-
-                fileWriter.write("${msg.javaClass.name} =>> $serialized")
+                // there's no need to include the class name in the output, since it's already in the packet (the packet id)
+                bufferedWriter.write(serialized)
             } catch (e: Exception) {
                 LOGGER.error("Error while serializing packet", e)
             }
         }
 
-        fileWriter.write("\n")
-        fileWriter.flush()
+        bufferedWriter.write("\n")
+
+        this.numWrites += 1
+        if (this.numWrites == 1000) {
+            // TODO: for performance, flush every 100 writes
+            bufferedWriter.flush()
+            LOGGER.info("Flushed $numWrites writes")
+            this.numWrites = 0
+        }
 
         ctx!!.fireChannelRead(msg)
     }
